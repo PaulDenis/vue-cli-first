2c5f1e32bb94a5c9e4eedf5a427c364264922db5
PaulDenis <d.paul@outlook.it>
2021-05-28 14:50:31 +0200

    Titolo creato

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>La mia prima App con Vue CLI</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Title'
+
+}
+</script>
+
+<style>
+
+</style>
